ca72d66f0d32f430797a55ce84ee55324aaa0339
ilya.ilyukhin <ilya.ilyukhin12@yandex.ru>
2024-03-19 00:45:47 +0300

    A function has been created to check all files in chosen directory

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ def create_rules_list(address):
     return yara_list
 
 
+def checking_files_in_directory(rules_for_checking, address):
+    directory_path = address
+    for filename in os.listdir(directory_path):
+        filepath = os.path.join(directory_path, filename)
+        if os.path.isfile(filepath):
+            matches_files = rules_for_checking.match(filepath)
+            if matches_files:
+                print(f"The file {filename} follows the following rules:")
+                for match in matches_files:
+                    print("\t", match)
+
+
 rules = yara.compile(filepaths=create_rules_list('rulesDir/'))
-matches = rules.match(filepath='text.txt')
-print(matches)
+checking_files_in_directory(rules, 'dataDir/')
+# matches = rules.match(filepath='dataDir/text.txt')
+
+# print(matches)
