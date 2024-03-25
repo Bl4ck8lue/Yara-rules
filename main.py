913af65074375041e878c33446ec673e41f37f55
Ilya Ilyukhin <ilya.ilyukhin12@yandex.ru>
2024-03-25 20:40:05 +0300

    Have finished the UI

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import sys
-from threading import Thread
+import time
 
 import yara
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import pyqtSignal, Qt
 from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton, QLabel, QMessageBox, QVBoxLayout, \
-    QFileDialog, QHBoxLayout, QProgressBar
+    QFileDialog, QHBoxLayout, QProgressBar, QLineEdit, QTextEdit
 from docx import Document
 
 
@@ -19,11 +19,12 @@ def create_rules_list(address):
 
 
 def print_in_document(path_to_check, matches):
-    count = 0
     # print(f"The file {path_to_check} follows the following rules:")
+    count = 0
     for match in matches:
 
         print("\t", match)
+        count += 1
 
         if os.path.exists(f'docxDir/{str(match)}.docx'):
             doc_to_write = Document(f'docxDir/{str(match)}.docx')
@@ -35,21 +36,27 @@ def print_in_document(path_to_check, matches):
             document.add_paragraph(f"The file {path_to_check}")
             document.save(f'docxDir/{str(match)}.docx')
 
+    return count
+
 
 def checking_files_in_directory(rules_for_checking, address):
     directory_path = address
+    x = 0
     for filename in os.listdir(directory_path):
         filepath = os.path.join(directory_path, filename)
         if os.path.isfile(filepath):
             matches_files = rules_for_checking.match(filepath)
             if matches_files:
-                print_in_document(filename, matches_files)
+                x = print_in_document(filename, matches_files)
+
+    return x
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         _signal = pyqtSignal(int)
+
         self.dir_list = None
         self.filename = None
         self.plainTextEdit = None
@@ -61,6 +68,7 @@ class MainWindow(QMainWindow):
 
         layout_first_card = QHBoxLayout()
         layout_second_card = QVBoxLayout()
+        layout_third_card = QVBoxLayout()
         layout_all = QVBoxLayout()
 
         # description of layout_first_card ----------------------------------------
@@ -84,19 +92,41 @@ class MainWindow(QMainWindow):
         # -------------------------------------------------------------------------
 
         # description of layout_second_card ----------------------------------------
+        self.non_editable_line_edit = QLineEdit(self)
+        self.non_editable_line_edit.setReadOnly(True)
+        # self.non_editable_line_edit.resize(150, 250)
+
         self.pbar = QProgressBar(self)
         self.pbar.setValue(0)
+        self.pbar.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.pbar.setStyleSheet("QProgressBar::chunk "
+                  "{"
+                    "background-color: pink;"
+                  "}")
 
         self.btn_to_scan = QPushButton("Сканировать")
         self.btn_to_scan.setCheckable(True)
         self.btn_to_scan.clicked.connect(self.the_button_was_clicked)
 
+        layout_second_card.addWidget(self.non_editable_line_edit)
         layout_second_card.addWidget(self.pbar)
         layout_second_card.addWidget(self.btn_to_scan)
         # --------------------------------------------------------------------------
 
+        # description of layout_third_card -----------------------------------------
+        result_all = QLabel("Результаты сканирования:")
+
+        self.result = QTextEdit(self)
+        self.result.resize(150, 100)
+
+        layout_third_card.addWidget(result_all)
+        layout_third_card.addWidget(self.result)
+
+        # --------------------------------------------------------------------------
+
         layout_all.addLayout(layout_first_card)
         layout_all.addLayout(layout_second_card)
+        layout_all.addLayout(layout_third_card)
 
         container = QWidget()
 
@@ -111,33 +141,50 @@ class MainWindow(QMainWindow):
                                                          "All Files(*);;Text Files(*.txt);;JPEG Files(*.jpeg);;\
                                                          PNG Files(*.png);;GIF File(*.gif)")
         if filename != '':
-            self.btn_to_choose_dir.setEnabled(False)
+            # self.btn_to_choose_dir.setEnabled(False)
+            self.non_editable_line_edit.setText(filename)
             self.filename = filename
 
     def the_btn_to_choose_dir_was_clicked(self):  # <-----
         dir_list = QFileDialog.getExistingDirectory(self, "Выбрать папку", ".")
         if dir_list != '':
-            self.btn_to_choose_file.setEnabled(False)
+            # self.btn_to_choose_file.setEnabled(False)
+            self.non_editable_line_edit.setText(dir_list)
+            # print(self.non_editable_line_edit.text())
             self.dir_list = dir_list
 
     def the_button_was_clicked(self):
 
-        self.thread = Thread()
-        self.thread._signal.connect(self.signal_accept)
-        self.thread.start()
-        self.btn_to_scan.setEnabled(False)
-
         rules = yara.compile(filepaths=create_rules_list('rulesDir/'))
 
-        if self.filename is None and self.dir_list != '':
-            checking_files_in_directory(rules, self.dir_list)
-        if self.filename != '' and self.dir_list is None:
-            matches = rules.match(self.filename)
-            filename = os.path.basename(self.filename).split('.')[0]
-            print_in_document(filename, matches)
+        for i in range(101):
+            # slowing down the loop
+            time.sleep(0.01)
 
-        self.btn_to_choose_file.setEnabled(True)
-        self.btn_to_choose_dir.setEnabled(True)
+            # setting value to progress bar
+            self.pbar.setValue(i)
+
+        file_or_dir_to_scan = self.non_editable_line_edit.text()
+        print(file_or_dir_to_scan)
+        if os.path.isdir(file_or_dir_to_scan):
+            count = checking_files_in_directory(rules, self.dir_list)
+            self.result.setText(f"Просканирована директория: {self.dir_list} . Выявлено угроз: {count}.")
+        else:
+            matches = rules.match(self.filename)
+            print(matches.values())
+            filename = os.path.basename(self.filename).split('.')[0]
+            count = print_in_document(filename, matches)
+            self.result.setText(f"Просканирован файл: {filename}. Выявлено угроз: {count}.")
+
+        # if self.filename is None and self.dir_list != '':
+        #     checking_files_in_directory(rules, self.dir_list)
+        # if self.filename != '' and self.dir_list is None:
+        #     matches = rules.match(self.filename)
+        #     if matches == {}:
+        #         print("No virus bytes")
+        #     else:
+        #         filename = os.path.basename(self.filename).split('.')[0]
+        #         print_in_document(filename, matches)
 
 
 app = QApplication(sys.argv)
